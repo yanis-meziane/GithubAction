3520feb1a01f19fa63d27f8532af30dd63e1d96c
Yanis Meziane <yanismeziane04@icloud.com>
2026-01-14 12:14:35 +0100

    Ajout fonction moyenne

--- job.py
+++ job.py
@@ -1,1 +1,11 @@
-a = 2 print("coucou", a)
+a = 2 
+print("coucou", a)
+
+def Moyenne(): 
+    tab = [10, 10, 10]
+    somme = sum(tab)
+    moyenne = somme/len(tab)
+    print("La moyenne des éléments du tableau est :", moyenne)
+
+Moyenne()
+
